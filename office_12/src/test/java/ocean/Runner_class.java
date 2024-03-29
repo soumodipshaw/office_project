a3d7db360d0330786a4ceb56e9a231ad4abcbcbd
Shaw <Shaw@DESKTOP-1K09BVS>
2024-03-29 12:31:07 +0530

    testing3

--- office_12/src/test/java/ocean/Runner_class.java
+++ office_12/src/test/java/ocean/Runner_class.java
@@ -39,5 +39,30 @@ public class Runner_class extends Base_class
 
 		 
 	} 
+	
+	@Test(dataProvider ="auto")
+	public void test2 (String ln,String csv) throws AWTException, InterruptedException 
+	{
+		Robot rbt = new Robot();
+		driver.findElement(By.xpath("//div[@class=' css-1xc3v61-indicatorContainer'][1]")).click();
+		driver.findElement(By.xpath("//input[@id='react-select-2-input']")).sendKeys("scamsearch.io");
+		rbt.keyPress(KeyEvent.VK_ENTER);
+		rbt.keyRelease(KeyEvent.VK_ENTER);
+		driver.findElement(By.xpath("//input[@id='google']")).click();
+		driver.findElement(By.name("last_name")).sendKeys(ln);
+		test.log(LogStatus.PASS, "pass");
+		driver.findElement(By.name("csv_name")).sendKeys(csv);
+		test.log(LogStatus.PASS, "pass");
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollBy(0,400)");
+		Thread.sleep(5000);
+		driver.findElement(By.id("searchBtn")).click();
+		Thread.sleep(60000);
+		rbt.keyPress(KeyEvent.VK_ENTER);
+		rbt.keyRelease(KeyEvent.VK_ENTER);
+
+		 
+	} 
+	
 
 }
